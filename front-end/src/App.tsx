3ac95a6d14bbab6e3902334c054d5335182a0971
Patrick Morton <patrickjamesmorton@gmail.com>
2025-04-29 15:26:00 +0100

    Added a carousel

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,18 +1,37 @@
+import useEmblaCarousel from 'embla-carousel-react'
+import { useState } from 'react'
+
 function App() {
+    const [emblaRef] = useEmblaCarousel({ loop: true })
+    const [trainingImages, setTrainingImages] = useState<string[]>([])
+
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const files = event.target.files
         if (files) {
-            console.log('Selected files:', files)
+            const trainingImageURLs = Array.from(files).map((file) =>
+                URL.createObjectURL(file)
+            )
+            setTrainingImages(trainingImageURLs)
         }
     }
 
     return (
         <div className="flex justify-center items-center h-screen">
-            <input
-                type="file"
-                onChange={handleFileChange}
-                multiple
-            />
+            {trainingImages.length > 0 && (
+                <div className="embla" ref={emblaRef}>
+                    <div className="embla__container">
+                        {trainingImages.map((image, index) => (
+                            <div className="embla__slide" key={index}>
+                                <img
+                                    src={image}
+                                    alt={`Training Image ${index + 1}`}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
+            <input type="file" onChange={handleFileChange} multiple />
         </div>
     )
 }
