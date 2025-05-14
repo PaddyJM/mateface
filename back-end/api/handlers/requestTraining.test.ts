82e1d62a114ce30355827e65cde873a26101bbd9
Patrick Morton <patrickjamesmorton@gmail.com>
2025-05-14 10:55:28 +0100

    Added further test

--- back-end/api/handlers/requestTraining.test.ts
+++ back-end/api/handlers/requestTraining.test.ts
@@ -65,4 +65,60 @@ describe('requestTraining', () => {
             trainingId: 'test',
         })
     })
+
+    it('should request training from the replicate API when there is no existing model', async () => {
+        const testEvent = {
+            username: 'test',
+            modelName: 'test',
+            s3Url: 'test',
+            taskToken: 'test',
+        }
+        const mockModel: Model = {
+            url: 'test',
+            owner: 'test',
+            name: 'test',
+            visibility: 'private',
+            run_count: 0,
+        }
+        mockReplicateClient.models.get.mockRejectedValueOnce(
+            new Error('Model not found')
+        )
+        mockReplicateClient.models.create.mockResolvedValueOnce(mockModel)
+        const mockTrainingModel: Model = {
+            url: 'test',
+            owner: 'test',
+            name: 'test',
+            visibility: 'private',
+            run_count: 0,
+            latest_version: {
+                id: 'test',
+                created_at: 'test',
+                cog_version: 'test',
+                openapi_schema: {},
+            },
+        }
+        mockReplicateClient.models.get.mockResolvedValueOnce(
+            mockTrainingModel
+        )
+        const mockTraining: Prediction = {
+            id: 'test',
+            version: 'test',
+            input: {},
+            output: {},
+            model: 'test',
+            created_at: 'test',
+            source: 'api',
+            status: 'succeeded',
+            urls: {
+                get: 'test',
+                cancel: 'test',
+            },
+        }
+        mockReplicateClient.trainings.create.mockResolvedValueOnce(mockTraining)
+        const handler = new RequestTraining(mockReplicateClient)
+        const response = await handler.requestTraining(testEvent)
+        expect(response).toEqual({
+            trainingId: 'test',
+        })
+    })
 })
