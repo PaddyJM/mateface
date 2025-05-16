ed0254d5207753cb8015c883e0dd6bfb4791c3cc
Patrick Morton <patrickjamesmorton@gmail.com>
2025-05-16 15:34:52 +0100

    Added test for request step function status behaviour

--- back-end/api/handlers/checkStatusTraining.test.ts
+++ back-end/api/handlers/checkStatusTraining.test.ts
@@ -1,11 +1,30 @@
-import { handler } from "./checkStatusTraining"
+import { handler } from './checkStatusTraining'
+import { createTestEvent } from '../lib/apiGatewayTestEvent'
+import { mockClient } from 'aws-sdk-client-mock'
+import { SFNClient, DescribeExecutionCommand } from '@aws-sdk/client-sfn'
 
-import { createTestEvent } from "../lib/apiGatewayTestEvent"
+const sfnMock = mockClient(SFNClient)
 
 describe('CheckStatusTrainingLambda', () => {
     it('should return a 200 status code if the execution is successful', async () => {
-        const testEvent = createTestEvent(JSON.stringify({ status: 'succeeded' }), {}, {})
+        const testEvent = createTestEvent('', {}, { executionArn: 'test' })
+        const startDate = new Date()
+        const stopDate = new Date()
+        sfnMock.on(DescribeExecutionCommand).resolves({
+            status: 'SUCCEEDED',
+            output: JSON.stringify({}),
+            startDate,
+            stopDate,
+        })
         const response = await handler(testEvent)
         expect(response.statusCode).toEqual(200)
+        expect(response.body).toEqual(
+            JSON.stringify({
+                status: 'SUCCEEDED',
+                output: {},
+                startDate,
+                stopDate,
+            })
+        )
     })
 })
